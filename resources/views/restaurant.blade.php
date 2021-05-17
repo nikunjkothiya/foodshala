da9a55290ccf488d5ec95dfa0cc010e9d5e8d199
Nikunj Patel <multipz.nikunjkothiya@gmail.com>
2021-05-17 10:03:42 +0530

    update

--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -13,17 +13,17 @@
     <title>Welcome Restaurant</title>
 
     <!-- Site Icons -->
-    <link rel="icon" href="//images/my-logo.jpg">
-    <link rel="apple-touch-icon" href="//images/apple-touch-icon.png">
+    <link rel="icon" href="{{asset('images/my-logo.jpg')}}">
+    <link rel="apple-touch-icon" href="{{asset('images/apple-touch-icon.png')}}">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="//css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
     <!-- Site CSS -->
-    <link rel="stylesheet" href="//css/style.css">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <!-- Responsive CSS -->
-    <link rel="stylesheet" href="//css/responsive.css">
+    <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="//css/custom.css">
+    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
 
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -100,19 +100,19 @@
     <a href="#" id="back-to-top" title="Back to top" style="display: none;"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></a>
 
     <!-- ALL JS FILES -->
-    <script src="//js/jquery-3.2.1.min.js"></script>
-    <script src="//js/popper.min.js"></script>
-    <script src="//js/bootstrap.min.js"></script>
+    <script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
+    <script src="{{asset('js/popper.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
     <!-- ALL PLUGINS -->
 
-    <script src="//js/jquery.superslides.min.js"></script>
-    <script src="//js/images-loded.min.js"></script>
-    <script src="//js/isotope.min.js"></script>
-    <script src="//js/baguetteBox.min.js"></script>
-    <script src="//js/jquery.mapify.js"></script>
-    <script src="//js/form-validator.min.js"></script>
-    <script src="//js/contact-form-script.js"></script>
-    <script src="//js/custom.js"></script>
+    <script src="{{asset('js/jquery.superslides.min.js')}}"></script>
+    <script src="{{asset('js/images-loded.min.js')}}"></script>
+    <script src="{{asset('js/isotope.min.js')}}"></script>
+    <script src="{{asset('js/baguetteBox.min.js')}}"></script>
+    <script src="{{asset('js/jquery.mapify.js')}}"></script>
+    <script src="{{asset('js/form-validator.min.js')}}"></script>
+    <script src="{{asset('js/contact-form-script.js')}}"></script>
+    <script src="{{asset('js/custom.js')}}"></script>
 
 </body>
 
